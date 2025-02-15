ae3710b46b738c674e46584e408227216834cf59
Shita Zeny <shita0639@gmail.com>
2025-02-15 10:30:23 +0700

    refactor: move queries folder into utils folder #3

---
+++ shita-hono/src/routes/index.ts
@@ -0,0 +1,48 @@
+//import hono
+import { Hono } from 'hono';
+import { createPerson, deletePerson, getPerson, getPersonById, updatePerson } from '../controllers/PersonController.js';
+import { jwt } from 'hono/jwt'
+import type { JwtVariables } from 'hono/jwt'
+import prisma from '../../prisma/client/index.js';
+import { apiKeyAuth } from '../middleware/auth.js';
+import { bearerAuth } from 'hono/bearer-auth';
+
+type Variables = JwtVariables
+
+const app = new Hono<{ Variables: Variables }>()
+
+// app.use('/*',jwt(
+//     {
+//       secret: '33c09648982ba1044f11365135a4a597c848f0bf28e4831578e24dc81cd1ad5b',
+//     }
+//   )
+// )
+
+const token = '33c09648982ba1044f11365135a4a597c848f0bf28e4831578e24dc81cd1ad5b'
+
+app.use('/*', bearerAuth({ token }))
+
+app.get('/shita', async (c) => {
+  const auth = await prisma.auth.findFirst()
+  if (auth) {
+      return c.json(
+          { 
+              statusCode: 200, 
+              message: 'Authorized',
+              key: auth.key 
+          }
+      )
+  }
+})
+
+app.use('*', apiKeyAuth)
+
+app.get('/data', (c) => getPerson(c));
+app.post('/data', (c) => createPerson(c));
+app.get('/data/:id', (c) => getPersonById(c));
+app.put('/data/:id', (c) => updatePerson(c)); // Biasain pake PUT 
+app.patch('/data/:id', (c) => updatePerson(c));
+app.delete('/data/:id', (c) => deletePerson(c));
+
+
+export const Routes = app;
